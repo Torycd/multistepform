4e3ad6d8f13fe13ab161efa1ed805a0a4c0f0f61
Torycd <vicidundun@gmail.com>
2024-04-16 10:34:59 +0100

    lets

--- src/Components/Steps/Step3.jsx
+++ src/Components/Steps/Step3.jsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import HeadStep from "../UI/HeadStep";
+import Button from "../UI/Button";
 
 export default function Step3() {
   return (
@@ -7,29 +9,44 @@ export default function Step3() {
         heading="Personal Info"
         subHeading="Add-ons help enhance your gaming experience."
       />
-      <div>
-        <div className="">
-          <input type="checkbox" />
-          <div>
-            <h2>Online service</h2>
-            <p>Access to multiplayer games</p>
+      <div className="flex flex-col gap-2">
+        <div className="flex justify-between p-5 rounded-lg border-2" >
+          <div className="flex gap-10">
+            <input type="checkbox" />
+            <div className="font-bold">
+              <h2>Online service</h2>
+              <p className="opacity-50">Access to multiplayer games</p>
+            </div>
           </div>
+          <p>+$1/mo</p>
         </div>
         {/*  */}
-        <div>
-        <input type="checkbox" />
-          <div>
-            <h2>Layer Storage</h2>
-            <p>Access to multiplayer games</p>
-          </div> 
+        <div className="flex justify-between p-5 rounded-lg border-2">
+          <div className="flex gap-10">
+            <input type="checkbox" className="checked:bg-purple-900" />
+            <div className="font-bold">
+              <h2>larger storage</h2>
+              <p className="opacity-50">Access to multiplayer games</p>
+            </div>
+          </div>
+          <p>+$2/mo</p>
         </div>
         {/*  */}
-        <div>
-        <input type="checkbox" />
-          <div>
-            <h2>Costomible profile.</h2>
-            <p>Access to multiplayer games</p>
+        <div className="flex justify-between p-5 rounded-lg border-2">
+          <div className="flex gap-10">
+            <input type="checkbox" />
+            <div className="font-bold">
+              <h2>Customizable Profile</h2>
+              <p className="opacity-50">Customize theme on your profile</p>
+            </div>
           </div>
+          <p>+$2/mo</p>
+        </div>
+        <div className="flex justify-between font-bold">
+          <Link to="/" className="px-4 py-3">
+            Go Back
+          </Link>
+          <Button type="submit" link="/stepfour" />
         </div>
       </div>
     </div>

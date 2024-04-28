cdaf2dd4f831aa9e06b99f663c9b55f626b196b9
Torycd <vicidundun@gmail.com>
2024-04-28 10:16:53 +0100

    ye

--- src/Components/Steps/Step4.jsx
+++ src/Components/Steps/Step4.jsx
@@ -5,40 +5,65 @@ import Button from "../UI/Button";
 
 export default function Step4() {
   const { summarys, plans, infos } = useContext(ActionContext);
-  
+
   return (
     <div className="space-y-10 py-10 h-full">
-      <HeadStep heading="Finishing up" subHeading="Double-check everything looks before confirming"/>
-      {/* Rendering plans */}
-      <ul>
-        {plans.map((item, index) => (
-          <li key={index}>
-            <h2>{item.plans.name}</h2>
-            <p>{item.plans.price}</p>
-          </li>
-        ))}
-      </ul>
-      {/* Rendering infos */}
-      <ul>
-        {infos.map((item, index) => (
-          <li key={index}>
-            <h2>Online Service: {item.infos.onlineService ? "Yes" : "No"}</h2>
-            <h2>Larger Storage: {item.infos.largerStorage ? "Yes" : "No"}</h2>
-            <h2>Customizable Profile: {item.infos.customizableProfile ? "Yes" : "No"}</h2>
-          </li>
-        ))}
-      </ul>
-      {/* Rendering summarys */}
-      <ul>
-        {summarys.map((item, index) => (
-          <li key={index}>
-            <h2>Name: {item.summary.name}</h2>
-            <p>Email: {item.summary.email}</p>
-            <p>Phone Number: {item.summary.number}</p>
-          </li>
-        ))}
-      </ul>
-      <Button type="submit"  butName="Confirm" link="/Congratulation"/>
+      <HeadStep
+        heading="Finishing up"
+        subHeading="Double-check everything looks before confirming"
+      />
+      <div className="bg-slate-200 p-5 rounded-lg space-y-3 text-xl font-bold ">
+        <ul className="border-b border-black">
+          {plans.map((item, index) => (
+            <li key={index} className="flex justify-between py-5">
+              <h2>{item.plans.name}</h2>
+              <p>{item.plans.price}</p>
+            </li>
+          ))}
+        </ul>
+
+        <ul>
+          {infos.map((item, index) => (
+            <li key={index} className="space-y-3 py-5">
+              {item.infos.onlineService ? (
+                <div className="flex justify-between">
+                  <h2>Online Service:</h2>
+                  <span>
+                    <p>+$1/mo</p>
+                  </span>
+                </div>
+              ) : null}
+              {item.infos.largerStorage ? (
+                <div className="flex justify-between">
+                  <h2>Larger Storage:</h2>
+                  <span>
+                    <p>+$2/mo</p>
+                  </span>
+                </div>
+              ) : null}
+              {item.infos.customizableProfile ? (
+                <div className="flex justify-between">
+                  <h2>Customizable Profile:</h2>
+                  <span>
+                    <p>+$3/mo</p>
+                  </span>
+                </div>
+              ) : null}
+            </li>
+          ))}
+        </ul>
+        <ul>
+          {summarys.map((item, index) => (
+            <li key={index}>
+              <h2>Name: {item.summary.name}</h2>
+              <p>Email: {item.summary.email}</p>
+              <p>Phone Number: {item.summary.number}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <Button type="submit" butName="Confirm" link="/Congratulation" />
     </div>
   );
 }

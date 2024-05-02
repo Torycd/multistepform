5ec7e3a2f26e725e381809b00ceff8e3be7fe04f
Torycd <vicidundun@gmail.com>
2024-05-02 07:52:39 +0100

    ok

--- src/Components/Steps/CongratStep.jsx
+++ src/Components/Steps/CongratStep.jsx
@@ -1,7 +1,7 @@
 const CongratStep = () => {
   return (
     <>
-      <div className="flex justify-start gap-10">
+      <div className="flex justify-start gap-10 h-full">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="80"
